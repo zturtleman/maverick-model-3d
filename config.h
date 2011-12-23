845d4f2e2a7f9f6bd8c7349ca4fcc536e2fb2517
Zack Middleton <ZTurtleMan@gmail.com>
2011-12-22 19:52:55 -0600

    Added config.h, don't need to use configure now.

---
+++ config.h
@@ -0,0 +1,17 @@
+#define PACKAGE "mm3d"
+#define VERSION "1.3.8"
+#define BYTEORDER 1234
+
+#ifndef WIN32
+
+/* Define when you have dlopen function */
+#define HAVE_DLOPEN
+
+/* Define when you have gettimeofday function */
+#define HAVE_GETTIMEOFDAY
+
+/* Define when you have Lua libs installed */
+/* #define HAVE_LUALIB */
+
+#endif
+
